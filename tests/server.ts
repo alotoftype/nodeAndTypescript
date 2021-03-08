4078e062ca6ae2cb8d8e9e01a8f891e957649485
Serge M <serge@tommyjohn.com>
2021-03-07 22:23:54 -0500

    creating first test

---
+++ tests/server.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+
+describe('Initial Test', ()=> {
+  it('checking to see if this is working', () => {
+    const sample = true;
+
+    expect(sample).to.equal(true)
+  })
+})
